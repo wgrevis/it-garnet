e784e3822c222f0fddcc75a11f180c1c24703b22
wgrevis <112125482+wgrevis@users.noreply.github.com>
2024-02-08 19:27:46 +0000

    cirlce work, fixes, etc

--- circle/assets/js/script.js
+++ circle/assets/js/script.js
@@ -6,12 +6,38 @@ $("CirlceForm").validate({
 
 //Prompt for Raduis Value
 
-function calcResults () {
+function displayResults() {
+    // if the form is valid, then make the calculations
+    if ($("#CircleForm").valid()) {
+        
+        document.getElementById("diameter").innerHTML = ""
+        document.getElementById("circumference").innerHTML = ""
+        document.getElementById("area").innerHTML = ""
 
+         var radius; // radius
+         var diameter; // diameter
+         var area; // area
+         var circumference; // circumference
 
+         // read in the legs as a string
+         radius = document.getElementById("radius").value;
+
+         // Convert numbers from strings to Floating Point
+         radius = parseFloat( radius ); 
+
+         // calculate the area, radius, circumference
+         area = calcArea(radius);
+         circumference = calcCircumference(radius);
+         diameter = calcDiameter(radius);
+
+         // display the results
+         document.getElementById("diameter").innerHTML = diameter.toString();
+         document.getElementById("circumference").innerHTML = circumference.toString();
+         document.getElementById("area").innerHTML = area.toString();
+    }
 }
 
-
+    
 // Calculations
 
 function calcDiameter(radius) {
@@ -26,6 +52,8 @@ function calcArea(radius) {
     return Math.PI * (radius*2)
 }
 
+
+// Display Results
 
 // Function to clear all inputs
 function clear() {
